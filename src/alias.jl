63b066261ccf45faf6f642da8712fc19082d3ca8
Y2P <yigit.pilavci01@gmail.com>
2019-12-20 12:15:06 +0100

    preprocess sparsified

--- src/alias.jl
+++ src/alias.jl
@@ -28,7 +28,7 @@ function alias_preprocess(g :: SimpleWeightedGraph)
         append!(ufull,setdiff(1:n, W.rowval[rn]))
         # For keeping probabilities
         # Initialize indices and stacks with original probabilities
-        for i = 1 : size(rn,1)
+        for i = 1 : size(rn)
             U[k,i] = n*probs[i]
             if(U[k,i] > 1)
                 push!(ofull,i)

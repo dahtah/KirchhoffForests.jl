f1dd1f84e7119d73553ed76933ce8f3ee1f65e7f
Guillaume Gautier <guillaume.gga@gmail.com>
2020-06-14 14:24:19 +0200

    Refactor /test, needed descriptions, and relative imports

--- test/aliasbenchmark.jl
+++ test/aliasbenchmark.jl
@@ -1,16 +1,21 @@
-using BenchmarkTools,SimpleWeightedGraphs,LightGraphs,RandomForests
-
-g = grid([50,50])
-# g = erdos_renyi(400,0.1)
-gw = SimpleWeightedGraph(g)
-gwp = PreprocessedWeightedGraph(gw)
+using BenchmarkTools, RandomForests
+using SimpleWeightedGraphs, LightGraphs
+#=
+TODO: describe the test
+=#
 
 println("Simple Graph")
-display(@benchmark random_forest($g,.1))
+g = grid([50, 50])
+# g = erdos_renyi(400,0.1)
+display(@benchmark random_forest($g, .1))
 println("")
+
 println("Weighted Graph")
-display(@benchmark random_forest($gw,.1))
+gw = SimpleWeightedGraph(g)
+display(@benchmark random_forest($gw, .1))
 println("")
+
 println("Preprocessed Weighted Graph")
-display(@benchmark random_forest($gwp,.1))
+gwp = PreprocessedWeightedGraph(gw)
+display(@benchmark random_forest($gwp, .1))
 println("")

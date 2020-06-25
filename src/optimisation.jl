ed1be9b2b29d96fd21493a16e37bc56977763603
Yusuf Yigit Pilavci <pilavciy@desktop-269.gipsa-lab.grenoble-inp.fr>
2020-06-25 17:07:51 +0200

    correction on newtons method

--- src/optimisation.jl
+++ src/optimisation.jl
@@ -1,14 +1,4 @@
-using LightGraphs
-using RandomForests
-using LinearAlgebra
-using SparseArrays
-using Plots
-using StatsBase
-using Clustering
-using Distributions
-using ImageView, TestImages, Gtk.ShortNames, Images, ImageMagick, ImageQualityIndexes
 
-plotly()
 
 function newton(G,y,t0,mu;α=0.001,numofiter = 100,tol=0.001, method="exact",nrep=100)
     t_k = copy(t0)
@@ -72,35 +62,3 @@ function irls(G,y,z0,mu;numofiter = 100,tol=0.001, method="exact",nrep=100)
 
     return z_k
 end
-
-
-imname = "lake_gray"
-im = imresize(testimage(imname), 64, 64)
-im = Int64.(floor.(Float64.(Gray24.(im))*256))
-nx = size(im,1)
-ny = size(im,2)
-rs = (v) -> reshape(v,nx,ny)
-G = LightGraphs.grid([nx,ny])
-im = im[:]
-for (idx,i) in enumerate(im)
-    im_noisy[idx] = rand(Poisson(i))
-end
-# imshow(reshape(im,nx,ny))
-x = zeros(size(Y))
-xtilde = zeros(size(Y))
-xbar = zeros(size(Y))
-# for i = 1 : k
-y = im_noisy
-z0 = exp.(rand(nv(G)))
-
-x = Int64.(floor.(irls(G,y,z0,1.0;numofiter = 10,tol=0.001, method="exact")))
-xtilde = Int64.(floor.(irls(G,y,z0,1.0;numofiter = 10,tol=0.001, method="xtilde",nrep=5000)))
-xbar = Int64.(floor.(irls(G,y,z0,1.0;numofiter = 10,tol=0.001, method="xbar",nrep=5000)))
-# end
-imshow(reshape(y,nx,ny))
-imshow(reshape(x,nx,ny))
-imshow(reshape(xtilde,nx,ny))
-imshow(reshape(xbar,nx,ny))
-# display(ImageQualityIndexes.assess_psnr(x, im))
-# display(ImageQualityIndexes.assess_psnr(xtilde, im))
-# display(ImageQualityIndexes.assess_psnr(xbar, im))

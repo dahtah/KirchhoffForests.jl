a15d80df5c847ca87aaeca44b5372af750a943b6
pilavciy <yusuf-yigit.pilavci@grenoble-inp.fr>
2020-06-25 22:29:40 +0200

    opt corrected

--- examples/opt_tools.jl
+++ examples/opt_tools.jl
@@ -1,3 +1,14 @@
+using LightGraphs
+using RandomForests
+using LinearAlgebra
+using SparseArrays
+using Plots
+using StatsBase
+using Clustering
+using Distributions
+using ImageView, TestImages, Gtk.ShortNames, Images, ImageMagick, ImageQualityIndexes
+
+plotly()
 
 function newton(G,y,t0,mu;α=0.001,numofiter = 100,tol=0.001, method="exact",nrep=100)
     t_k = copy(t0)
@@ -61,3 +72,40 @@ function irls(G,y,z0,mu;numofiter = 100,tol=0.001, method="exact",nrep=100)
 
     return z_k
 end
+
+
+imname = "lake_gray"
+im = imresize(testimage(imname), 64, 64)
+im = Int64.(floor.(Float64.(Gray24.(im))*256))
+nx = size(im,1)
+ny = size(im,2)
+rs = (v) -> reshape(v,nx,ny)
+G = LightGraphs.grid([nx,ny])
+im = im[:]
+im_noisy = zeros(size(im))
+for (idx,i) in enumerate(im)
+    im_noisy[idx] = rand(Poisson(i))
+end
+# imshow(reshape(im,nx,ny))
+x = zeros(size(im))
+xtilde = zeros(size(im))
+xbar = zeros(size(im))
+# for i = 1 : k
+y = im_noisy
+z0 = exp.(rand(nv(G)))
+
+x = Int64.(floor.(newton(G,y,z0,1.0;numofiter = 100,tol=0.001, method="exact")))
+xtilde = Int64.(floor.(newton(G,y,z0,1.0;numofiter = 100,tol=0.001, method="xtilde",nrep=10)))
+xbar = Int64.(floor.(newton(G,y,z0,1.0;numofiter = 100,tol=0.001, method="xbar",nrep=10)))
+# end
+Gray.(reshape(y./256,nx,ny))
+Gray.(reshape(x./256,nx,ny))
+Gray.(reshape(xtilde./256,nx,ny))
+Gray.(reshape(xbar./256,nx,ny))
+
+# imshow(reshape(x,nx,ny))
+# imshow(reshape(xtilde,nx,ny))
+# imshow(reshape(xbar,nx,ny))
+# display(ImageQualityIndexes.assess_psnr(x, im))
+# display(ImageQualityIndexes.assess_psnr(xtilde, im))
+# display(ImageQualityIndexes.assess_psnr(xbar, im))

b796e5b42c6cd93791e8507b69deb6821bac8dca
pilavciy <yusuf-yigit.pilavci@grenoble-inp.fr>
2020-06-28 12:18:20 +0200

    opt corrected

--- examples/opt_tools.jl
+++ examples/opt_tools.jl
@@ -9,79 +9,10 @@ using Distributions
 using ImageView, TestImages, Gtk.ShortNames, Images, ImageMagick, ImageQualityIndexes
 
 plotly()
-#
-# function newton(G,y,t0,mu;α=0.001,numofiter = 100,tol=0.001, method="exact",nrep=100)
-#     t_k = copy(t0)
-#     tprev = copy(t0)
-#     increment = norm(t0)
-#     L = laplacian_matrix(G)
-#     k = 0
-#
-#     while( increment > tol && k < numofiter)
-#         vec_k = 1.0 .- (y ./ exp.(t_k)) + ((L*t_k)./(mu .* exp.(t_k)))
-#         tprev = copy(t_k)
-#         println("Iteration $k, increment $increment")
-#
-#         if (method=="exact")
-#             t_k -= α*(smooth(G,mu .* exp.(t_k),vec_k))
-#         else
-#             vecbar = repeat(mean(vec_k, dims=1), outer=size(y,1))
-#             Δ = -vecbar + vec_k
-#             if (method=="xtilde")
-#                 t_k -= α*(vecbar + smooth_rf(G,1 ./ (mu .* exp.(t_k)),Δ,[];nrep=nrep,variant=1).est)
-#             elseif (method=="xbar")
-#                 t_k -= α*(vecbar + smooth_rf(G,1 ./ (mu .* exp.(t_k)),Δ,[];nrep=nrep,variant=2).est)
-#             end
-#         end
-#         k += 1
-#         increment = norm(tprev - t_k)
-#     end
-#     println("Method: $method. Terminated after $k iterations, increment $increment")
-#     return exp.(t_k)
-# end
-
-# function irls_(G,y,z0,mu; numofiter = 100,tol=0.001, method="exact",nrep=100)
-#     B = incidence_matrix(G,oriented=true)
-#     k = 0
-#     increment = norm(z0)
-#     z_k = copy(z0)
-#     z_prev = copy(z0)
-#     while( increment > tol && k < numofiter  )
-#         zprev = copy(z_k)
-#         update =(abs.(B'*z_k) .+ 0.1).^(-1)
-#         M_k = spdiagm(0 => (update))
-#
-#         println("Method: $method,Iteration $k ,Increment: $increment. ")
-#
-#         # if(norm(update) == Inf)
-#         #     break
-#         # end
-#         L_k = B*M_k*(B')
-#         L_k[diagind(L_k)] .= 0
-#         G = SimpleWeightedGraph(-(L_k + L_k')./2)
-#
-#         if (method=="exact")
-#             z_k = (smooth(G,mu,y))
-#         else
-#             ybar = repeat(mean(y, dims=1), outer=size(y,1))
-#             Δ = -ybar + y
-#             if (method=="xtilde")
-#                 z_k = (ybar + smooth_rf(G,mu,Δ,[];nrep=nrep,variant=1).est)
-#             elseif (method=="xbar")
-#                 z_k = (ybar + smooth_rf(G,mu,Δ,[];nrep=nrep,variant=2).est)
-#             end
-#         end
-#         k += 1
-#         increment = norm(zprev - z_k)
-#     end
-#     println("Method: $method. Terminated after $k iterations, increment $increment")
-#
-#     return z_k
-# end
 
 
 imname = "lake_gray"
-im = imresize(testimage(imname), 10, 10)
+im = imresize(testimage(imname), 32, 32)
 im = Int64.(floor.(Float64.(Gray24.(im))*256))
 nx = size(im,1)
 ny = size(im,2)
@@ -93,37 +24,15 @@ im_noisy = zeros(size(im))
 for (idx,i) in enumerate(im)
     im_noisy[idx] =  rand(Poisson(i))
 end
-# imshow(reshape(im,nx,ny))
+
 x = zeros(size(im))
 xtilde = zeros(size(im))
 xbar = zeros(size(im))
-# for i = 1 : k
 y = im_noisy
-
 z0 = (rand(nv(G)))
 
-x = ((newton(G,y,z0,1.0;numofiter = 100,tol=0.001, method="exact")))
-xtilde = ((newton(G,y,z0,1.0;numofiter = 100,tol=0.001, method="xtilde",nrep=100)))
-xbar = ((newton(G,y,z0,1.0;numofiter = 100,tol=0.001, method="xbar",nrep=100)))
-# end
-Gray.(reshape(y./256,nx,ny))
-Gray.(reshape(x./256,nx,ny))
-# Gray.(reshape(xtilde./256,nx,ny))
-# Gray.(reshape(xbar./256,nx,ny))
-# Gray.(reshape(a./256,nx,ny))
+x = ((newton(G,y,z0,0.01;α=0.05,numofiter = 100,tol=0.001, method="exact")))
+xtilde = ((newton(G,y,z0,0.01;α=0.05,numofiter = 100,tol=0.001, method="xtilde",nrep=20)))
+xbar = ((newton(G,y,z0,0.01;α=0.05,numofiter = 100,tol=0.001, method="xbar",nrep=20)))
 
-plot(y)
-plot!(x)
-plot!(xtilde)
-plot!(xbar)
-# imshow(reshape(x,nx,ny))
-# imshow(reshape(xtilde,nx,ny))
-# imshow(reshape(xbar,nx,ny))
-display(norm(im-y)./norm(im))
-display(norm(im-x)./norm(im))
-display(norm(im-xtilde)./norm(im))
-display(norm(im-xbar)./norm(im))
-
-# display(ImageQualityIndexes.assess_psnr(x, im))
-# display(ImageQualityIndexes.assess_psnr(xtilde, im))
-# display(ImageQualityIndexes.assess_psnr(xbar, im))
+Gray.([reshape(y./256,nx,ny) reshape(x./256,nx,ny) reshape(xtilde./256,nx,ny) reshape(xbar./256,nx,ny)])
--- src/optimisation.jl
+++ src/optimisation.jl
@@ -1,5 +1,5 @@
 
-function newton(G,y,t0,mu;α=0.001,numofiter = 100,tol=0.001, method="exact",nrep=100)
+function newton(G,y,t0,mu;α=0.1,numofiter = 100,tol=0.001, method="exact",nrep=100,status=true)
     t_k = copy(t0)
     tprev = copy(t0)
     increment = norm(t0)
@@ -10,8 +10,9 @@ function newton(G,y,t0,mu;α=0.001,numofiter = 100,tol=0.001, method="exact",nre
         vec_k = 1.0 .- (y ./ exp.(t_k)) + ((L*t_k)./(mu .* exp.(t_k)))
         q =  (mu .* exp.(t_k))
         tprev = copy(t_k)
-        println("Iteration $k, increment $increment")
-
+        if(status == true)
+            println("Iteration $k, increment $increment")
+        end
         if (method=="exact")
             t_k -= α*(smooth(G,q,vec_k))
         else
@@ -30,7 +31,7 @@ function newton(G,y,t0,mu;α=0.001,numofiter = 100,tol=0.001, method="exact",nre
     return exp.(t_k)
 end
 
-function irls(G,y,z0,mu;numofiter = 100,tol=0.001, method="exact",nrep=100)
+function irls(G,y,z0,mu;numofiter = 100,tol=0.001, method="exact",nrep=100,status=true)
     B = incidence_matrix(G,oriented=true)
     k = 0
     increment = norm(z0)
@@ -41,7 +42,9 @@ function irls(G,y,z0,mu;numofiter = 100,tol=0.001, method="exact",nrep=100)
         update =(abs.(B'*z_k) .+ 0.0001).^(-1)
         M_k = spdiagm(0 => (update))
 
-        println("Iteration $k ,Increment: $increment. ")
+        if(status == true)
+            println("Iteration $k, increment $increment")
+        end
 
         L_k = B*M_k*(B')
         L_k[diagind(L_k)] .= 0
